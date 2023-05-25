479931f4b68e73b4c30b73ec408a6137a1946af4
HanMyeongJun <81223415+hm0874@users.noreply.github.com>
2023-05-26 01:37:14 +0900

    myeongjuntest

---
+++ app/src/main/java/com/example/jj_club/activities/MBTI/MbtiTestQ5Activity.java
@@ -0,0 +1,45 @@
+package com.example.jj_club.activities.MBTI;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.jj_club.R;
+
+public class MbtiTestQ5Activity extends AppCompatActivity {
+
+    private Button btnLogin1;
+    private Button btnLogin2;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mbti_test_q5);
+
+        btnLogin1 = findViewById(R.id.button_login);
+        btnLogin2 = findViewById(R.id.button);
+
+        btnLogin1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 버튼1이 클릭되었을 때 mbti_test_q2.xml 페이지로 이동
+                Intent intent = new Intent(MbtiTestQ5Activity.this, MbtiTestQ6Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnLogin2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 버튼2가 클릭되었을 때 mbti_test_q2.xml 페이지로 이동
+                Intent intent = new Intent(MbtiTestQ5Activity.this, MbtiTestQ6Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
